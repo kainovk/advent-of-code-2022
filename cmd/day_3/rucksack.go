f470abc1aec180e06fe0fec26914a31a375eb671
Kirill Kainov <kainov.dev@gmail.com>
2022-12-05 19:02:32 +0300

    day 3 part 2

--- cmd/day_3/rucksack.go
+++ cmd/day_3/rucksack.go
@@ -12,6 +12,8 @@ func main() {
 	sc := bufio.NewScanner(file)
 
 	totalSum := 0
+	var group []string
+	commonItemPrioritySum := 0
 	for sc.Scan() {
 		line := sc.Text()
 		half := len(line) / 2
@@ -19,8 +21,41 @@ func main() {
 		right := line[half:]
 
 		totalSum += sumDuplicatesPriorities(left, right)
+
+		group = append(group, line)
+		if len(group) == 3 {
+			commonItemPrioritySum += calculateCommonItemPriority(group)
+			group = nil
+		}
 	}
 	fmt.Println(totalSum)
+	fmt.Println(commonItemPrioritySum)
+}
+
+func calculateCommonItemPriority(group []string) int {
+	cMap := make(map[rune]byte)
+	for _, s := range group {
+		set := toRuneSet(s)
+		for r := range set {
+			cMap[r]++
+		}
+	}
+
+	result := 0
+	for r, c := range cMap {
+		if c == 3 {
+			result += priority(r)
+		}
+	}
+	return result
+}
+
+func toRuneSet(s string) map[rune]bool {
+	set := make(map[rune]bool)
+	for _, r := range s {
+		set[r] = true
+	}
+	return set
 }
 
 func sumDuplicatesPriorities(left, right string) int {
@@ -46,9 +81,9 @@ func sumDuplicatesPriorities(left, right string) int {
 func priority(r rune) int {
 	p := 0
 	if r <= 90 {
-		p = int(r - rune(38))
+		p = int(r) - 38
 	} else {
-		p = int(r - rune(96))
+		p = int(r) - 96
 	}
 	return p
 }
