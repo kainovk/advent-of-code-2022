cab735f6aff8ede293af4b3fea991fa8b0879b29
Kirill Kainov <kainov.dev@gmail.com>
2022-12-05 12:12:44 +0300

    day 2 part 2

--- cmd/day_2/main.go
+++ cmd/day_2/main.go
@@ -11,27 +11,27 @@ func main() {
 	defer file.Close()
 	sc := bufio.NewScanner(file)
 
-	offset := 23
 	total := 0
 	for sc.Scan() {
 		line := []byte(sc.Text())
-		opp := int(line[0])
-		me := int(line[2])
-		diff := me - (opp + offset)
+		opp := int(line[0]+1) % 3
+		me := int(line[2]+2) % 3
 
-		points := 1
-		if diff == 0 {
-			points += 3
-		} else if diff == 1 || diff == -2 {
-			points += 6
-		}
-
-		if me == 'Y' {
-			points += 1
-		} else if me == 'Z' {
-			points += 2
-		}
-		total += points
+		total += calculatePoints(opp, me)
 	}
 	fmt.Println(total)
+}
+
+func calculatePoints(opp, me int) int {
+	result := 0
+	if me == 0 {
+		result += (opp + 2) % 3
+	} else if me == 1 {
+		result += 3
+		result += opp
+	} else {
+		result += 6
+		result += (opp + 1) % 3
+	}
+	return result + 1
 }
