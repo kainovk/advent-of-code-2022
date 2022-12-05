e47b95a86dda667e72a9ab00d87f31a51396d73f
Kirill Kainov <kainov.dev@gmail.com>
2022-12-05 23:14:19 +0300

    day 4 part 2

--- cmd/day_4/cleanup.go
+++ cmd/day_4/cleanup.go
@@ -13,15 +13,34 @@ func main() {
 	defer file.Close()
 	sc := bufio.NewScanner(file)
 
-	result := 0
+	cFullyOverlaps := 0
+	cOverlaps := 0
 	for sc.Scan() {
 		line := sc.Text()
 		parts := strings.Split(line, ",")
 		if fullyContains(parts[0], parts[1]) {
-			result++
+			cFullyOverlaps++
+		}
+		if overlaps(parts[0], parts[1]) {
+			cOverlaps++
 		}
 	}
-	fmt.Println(result)
+	fmt.Println(cFullyOverlaps)
+	fmt.Println(cOverlaps)
+}
+
+func overlaps(s1, s2 string) bool {
+	elf1 := strings.Split(s1, "-")
+	elf2 := strings.Split(s2, "-")
+	start1, _ := strconv.ParseInt(elf1[0], 10, 64)
+	start2, _ := strconv.ParseInt(elf2[0], 10, 64)
+	end1, _ := strconv.ParseInt(elf1[1], 10, 64)
+	end2, _ := strconv.ParseInt(elf2[1], 10, 64)
+	if start1 <= end2 && start2 <= end1 ||
+		start1 <= start2 && end2 <= end1 {
+		return true
+	}
+	return false
 }
 
 func fullyContains(s1, s2 string) bool {
