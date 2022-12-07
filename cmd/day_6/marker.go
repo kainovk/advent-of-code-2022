a5b78cc0b4a0e19d1c8b27fbd855a4f757dbe71b
Kirill Kainov <kainov.dev@gmail.com>
2022-12-07 12:11:51 +0300

    day 6 part 1

---
+++ cmd/day_6/marker.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, _ := os.Open("cmd/day_6/input.txt")
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+	sc.Split(bufio.ScanRunes)
+
+	const seq = 4
+	var queue []rune
+	for i := 0; i < seq-1; i++ {
+		sc.Scan()
+		char := rune(sc.Text()[0])
+		queue = append(queue, char)
+	}
+
+	result := seq - 1
+	for sc.Scan() {
+		result++
+		char := rune(sc.Text()[0])
+		queue = append(queue, char)
+		if len(queue) == seq+1 {
+			queue = queue[1:]
+		}
+		if len(toSet(queue)) == seq {
+			break
+		}
+	}
+	fmt.Println(result)
+}
+
+func toSet(q []rune) map[rune]bool {
+	set := make(map[rune]bool)
+	for _, r := range q {
+		set[r] = true
+	}
+	return set
+}
