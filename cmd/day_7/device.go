2e07dc62b2e8abf9ffa8a87549c1af845a80a775
Kirill Kainov <kainov.dev@gmail.com>
2022-12-07 15:01:11 +0300

    day 7 part 1

---
+++ cmd/day_7/device.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type directory struct {
+	name    string
+	parent  *directory
+	subdirs []*directory
+	fsize   int
+	dsize   int
+}
+
+func main() {
+	file, _ := os.Open("cmd/day_7/input.txt")
+	defer file.Close()
+	sc := bufio.NewScanner(file)
+
+	root := directory{name: "/", parent: nil}
+	var curDir = &root
+
+	for sc.Scan() {
+		line := strings.Split(sc.Text(), " ")
+
+		// Build the device directory structure.
+		if line[0] == "$" && line[1] == "cd" {
+			if line[2] == ".." {
+				curDir = curDir.parent
+			} else if line[2] != "/" {
+				index := findDirectoryIndex(curDir.subdirs, line[2])
+				curDir = curDir.subdirs[index]
+			}
+		} else if line[0] == "dir" {
+			curDir.subdirs = append(curDir.subdirs,
+				&directory{name: line[1], parent: curDir})
+		} else {
+			fsize, _ := strconv.Atoi(line[0])
+			curDir.fsize += fsize
+		}
+	}
+	setDirectoriesSize(&root)
+
+	fmt.Println("Total size: ", root.fsize+root.dsize)
+	result1 := partOne(root)
+	fmt.Println(result1)
+}
+
+func partOne(root directory) int {
+	result := 0
+	if root.fsize+root.dsize <= 100000 {
+		result += root.fsize + root.dsize
+	}
+	for _, dir := range root.subdirs {
+		result += partOne(*dir)
+	}
+	return result
+}
+
+func setDirectoriesSize(root *directory) {
+	iter := &root
+	for _, dir := range (*iter).subdirs {
+		(*iter).dsize += setDirectorySize(dir)
+	}
+}
+
+func setDirectorySize(dir *directory) int {
+	iter := &dir
+	for _, subDir := range (*iter).subdirs {
+		dir.dsize += setDirectorySize(subDir)
+	}
+	return dir.fsize + dir.dsize
+}
+
+func findDirectoryIndex(subdirs []*directory, dName string) int {
+	for i, dir := range subdirs {
+		if dir.name == dName {
+			return i
+		}
+	}
+	return -1
+}
