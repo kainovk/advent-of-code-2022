510e03581a67c8c1291cd5999882c1a539565645
Kirill Kainov <kainov.dev@gmail.com>
2022-12-10 13:40:50 +0300

    day 9 part 2

--- cmd/day_9/bridge.go
+++ cmd/day_9/bridge.go
@@ -11,22 +11,30 @@ import (
 
 type position struct {
 	x, y int
+	last *position
 }
 
 func (p position) near(pp position) bool {
 	return (p.x-pp.x)*(p.x-pp.x) <= 1 && (p.y-pp.y)*(p.y-pp.y) <= 1
 }
 
+func (p position) diagonal(pp position) bool {
+	return (p.x-pp.x)*(p.x-pp.x) == 4 && (p.y-pp.y)*(p.y-pp.y) == 4
+}
+
 var (
 	visited = make(map[position]bool)
-	hPos    = position{0, 0}
-	tPos    = position{0, 0}
+	hPos    = position{0, 0, nil}
 )
+
+const knots = 10 - 1
 
 func main() {
 	file, _ := os.Open("cmd/day_9/input.txt")
 	defer file.Close()
 	sc := bufio.NewScanner(file)
+
+	makeRope()
 
 	visited[hPos] = true
 	for sc.Scan() {
@@ -34,61 +42,135 @@ func main() {
 		m := strings.Split(line, " ")
 		vec := mapToPosition(m[0])
 		steps, _ := strconv.Atoi(m[1])
-		move := position{vec.x * steps, vec.y * steps}
+		move := position{vec.x * steps, vec.y * steps, nil}
 		makeMove(move)
+		printRope()
 	}
-	fmt.Println("Head position:", hPos)
-	fmt.Println("Tail position:", tPos)
 	fmt.Println("Visited:", len(visited))
 }
 
+func printRope() {
+	iter := &hPos
+	for i := 0; i < knots; i++ {
+		fmt.Println("i:", i, "x:", iter.x, "y:", iter.y)
+		iter = iter.last
+	}
+	fmt.Println("i:", knots, "x:", iter.x, "y:", iter.y)
+	fmt.Println("---")
+}
+
+func makeRope() {
+	iter := &hPos
+	for i := 0; i < knots; i++ {
+		iter.last = &position{0, 0, nil}
+		iter = iter.last
+	}
+}
+
 func makeMove(move position) {
-	hPos.x += move.x
-	hPos.y += move.y
-	if !hPos.near(tPos) {
-		if math.Abs(float64(hPos.x-tPos.x)) <= 1 {
-			tPos.x = hPos.x
-		} else {
-			tPos.y = hPos.y
+	if move.x == 0 {
+		for move.y != 0 {
+			makeMoveY(move.y)
+			move.y += -1 * move.y / int(math.Abs(float64(move.y)))
+			cling()
 		}
-		if hPos.x == tPos.x {
-			if hPos.y > tPos.y {
-				for hPos.y-1 != tPos.y {
-					tPos.y++
-					visited[tPos] = true
+	} else {
+		for move.x != 0 {
+			makeMoveX(move.x)
+			move.x += -1 * move.x / int(math.Abs(float64(move.x)))
+			cling()
+		}
+	}
+}
+
+func cling() {
+	iter := &hPos
+	for i := 0; i < knots; i++ {
+		last := iter.last
+		if !iter.near(*last) && !iter.diagonal(*last) {
+			if math.Abs(float64(iter.x-last.x)) <= 1 {
+				last.x = iter.x
+			} else {
+				last.y = iter.y
+			}
+			if iter.x == last.x {
+				if iter.y > last.y {
+					for iter.y-1 != last.y {
+						last.y++
+						if last.last == nil {
+							visited[*last] = true
+						}
+					}
+				} else {
+					for iter.y+1 != last.y {
+						last.y--
+						if last.last == nil {
+							visited[*last] = true
+						}
+					}
 				}
 			} else {
-				for hPos.y+1 != tPos.y {
-					tPos.y--
-					visited[tPos] = true
+				if iter.x > last.x {
+					for iter.x-1 != last.x {
+						last.x++
+						if last.last == nil {
+							visited[*last] = true
+						}
+					}
+				} else {
+					for iter.x+1 != last.x {
+						last.x--
+						if last.last == nil {
+							visited[*last] = true
+						}
+					}
 				}
 			}
-		} else {
-			if hPos.x > tPos.x {
-				for hPos.x-1 != tPos.x {
-					tPos.x++
-					visited[tPos] = true
-				}
+		} else if iter.diagonal(*last) {
+			if iter.x > last.x {
+				last.x++
 			} else {
-				for hPos.x+1 != tPos.x {
-					tPos.x--
-					visited[tPos] = true
-				}
+				last.x--
+			}
+			if iter.y > last.y {
+				last.y++
+			} else {
+				last.y--
+			}
+			if last.last == nil {
+				visited[*last] = true
 			}
 		}
+		iter = last
+	}
+}
+
+func makeMoveX(x int) {
+	if x > 0 {
+		hPos.x++
+	} else {
+		hPos.x--
+	}
+}
+
+func makeMoveY(y int) {
+	if y > 0 {
+		hPos.y++
+	} else {
+		hPos.y--
 	}
 }
 
 func mapToPosition(s string) position {
 	switch s {
 	case "U":
-		return position{0, 1}
+		return position{0, 1, nil}
 	case "D":
-		return position{0, -1}
+		return position{0, -1, nil}
 	case "L":
-		return position{-1, 0}
+		return position{-1, 0, nil}
 	case "R":
-		return position{1, 0}
+		return position{1, 0, nil}
 	}
-	return position{0, 0}
+	return position{0, 0, nil}
 }
